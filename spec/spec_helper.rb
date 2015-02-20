1c6bce8389c4b454e49e5e4111c1820d6887ac16
Dinesh Vasudevan <dineshvasudevan2003@yahoo.com>
2015-02-20 12:41:40 +0100

    Documentation format by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,7 @@ RSpec.configure do |config|
     # Use the documentation formatter for detailed output,
     # unless a formatter has already been configured
     # (e.g. via a command-line flag).
-    config.default_formatter = "doc"
+    config.default_formatter = "documentation"
   end
 
   # config.order = :random
